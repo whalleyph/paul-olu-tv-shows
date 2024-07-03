303105a182da98b74122b29082bbfa73029b434f
2405-011-pw <walypa56@gmail.com>
2024-07-03 11:51:22 +0100

    6 episodes on page

--- src/components/TvShowsApp.jsx
+++ src/components/TvShowsApp.jsx
@@ -2,17 +2,19 @@ import episodes from "../data/gameOfThronesEpisodes.json";
 import { EpisodeCard } from "./EpisodeCard";
 export function TVShowsApp() {
     const ep1 = episodes[0];
+    const episodeSlice = episodes.slice(0, 6);
 
-    const summaryHTML = {
-        __html: ep1.summary,
-    };
+    const episodeList = episodeSlice.map((episode) => {
+        return <EpisodeCard episode={episode} key={episode.id} />;
+    });
 
     return (
         <main className="tvShowsApp">
             <h1>{ep1._links.show.name}</h1>
-            <EpisodeCard
-            episode={ep1} 
-            />
+            <p>
+                Data from <a href="TVMaze.com">TVMaze.com</a>
+            </p>
+            {episodeList}
         </main>
     );
 }
