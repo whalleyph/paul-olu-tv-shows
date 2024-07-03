a5c6101fb8d7bc9cefdecb18382ab5f659ed5776
2405-011-pw <walypa56@gmail.com>
2024-07-03 10:17:42 +0100

    prepare document

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { DemonstratingProps } from "./components/demo/DemonstratingProps";
 import { TVShowsApp } from "./components/TvShowsApp.jsx";
 
 function App() {
     return (
         <>
             <TVShowsApp />
-            <DemonstratingProps />
         </>
     );
 }
