48279afc82a69da56d24c2a2d9042af3d415df1f
2405-011-pw <walypa56@gmail.com>
2024-07-03 10:48:46 +0100

    made template structure episode 1

--- src/components/TvShowsApp.jsx
+++ src/components/TvShowsApp.jsx
@@ -1,11 +1,22 @@
 import episodes from "../data/gameOfThronesEpisodes.json";
 export function TVShowsApp() {
     const ep1 = episodes[0];
+
+    const summaryHTML = {
+        __html: ep1.summary,
+    };
+
     return (
         <main className="tvShowsApp">
-            <h1>TVShowsApp</h1>
-            <div>Got {episodes.length} episode(s) in array</div>
-            <div>Title of first episode: {ep1.name}</div>
+            <h1>Game of Thrones Episodes</h1>
+            <div className="tvShowsCard">
+                <h2>{ep1.name}</h2>
+                <img src={ep1.image.medium} />
+                <div
+                    className="episode-summary"
+                    dangerouslySetInnerHTML={summaryHTML}
+                />
+            </div>
         </main>
     );
 }
